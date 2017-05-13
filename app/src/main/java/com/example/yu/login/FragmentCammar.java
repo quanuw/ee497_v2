5e3ee7af2e25b33cf19d85277f9e69e4e0a29513
amandatran2 <amanda.tran02@gmail.com>
2017-05-12 20:46:42 -0700

    email verification

--- app/src/main/java/com/example/yu/login/FragmentCammar.java
+++ app/src/main/java/com/example/yu/login/FragmentCammar.java
@@ -2,42 +2,54 @@ package com.example.yu.login;
 
 
 import android.content.Intent;
-import android.graphics.drawable.Drawable;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.media.MediaScannerConnection;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.FileProvider;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 
 import java.io.File;
+import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-import static android.content.ContentValues.TAG;
+import static android.app.Activity.RESULT_OK;
+
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class FragmentCammar extends Fragment {
 
+    private static final String TAG = "Camera";
+    private static final int REQUEST_IMAGE_CAPTURE = 1;
 
     public FragmentCammar() {
         // Required empty public constructor
     }
 
-    private Button click;
-    private Button shareButton;
+    private ImageButton click;
+    private ImageButton shareButton;
 
-    private ImageView imageView;
 
     private Uri mediaStoreUri = null; //for sharing
+
+    private String fileName;
+
+    private File imageFile;
+
+    private Uri pictureFileUri; //for saving
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -45,25 +57,56 @@ public class FragmentCammar extends Fragment {
         // Inflate the layout for this fragment
         View rootview = inflater.inflate(R.layout.fragment_fragment_cammar, container, false);
 
-        click = (Button) rootview.findViewById(R.id.CammarButton);
-        imageView  = (ImageView) rootview.findViewById(R.id.CammarView);
+        click = (ImageButton) rootview.findViewById(R.id.CammarButton);
+
 
         // Take picture
         // TODO: Picture is being saved upside down.
         click.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                File file = getFile();
-                Uri pictureFileUri = Uri.fromFile(file);
-                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                if (takePictureIntent.resolveActivity(getActivity().getPackageManager()) != null) {
+                    File file = null;
 
-                intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(file));
-                startActivityForResult(intent , 1);
+                    try {
+                        String timestamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date()); //include timestamp
+
+//                        File dir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+                        File dir = getActivity().getExternalFilesDir(Environment.DIRECTORY_DOCUMENTS);
+                        if (!dir.exists()) {
+                            dir.mkdirs(); //make Documents directory if doesn't otherwise exist
+                        }
+                        file = new File(dir, "PIC_" + timestamp + ".jpg");
+                        boolean created = file.createNewFile(); //actually make the file!
+                        Log.v(TAG, "File created: " + created);
+
+                    } catch (IOException ioe) {
+                        Log.d(TAG, "IOE CAMERA: " + Log.getStackTraceString(ioe));
+                    }
+
+                    if (file != null) {
+                        pictureFileUri = FileProvider.getUriForFile(getActivity(), BuildConfig.APPLICATION_ID, file);
+                        Log.v(TAG, "Uri: "+pictureFileUri);
+
+                        //for sharing the media (produces a Uri the Messenger has permissions for)
+                        MediaScannerConnection.scanFile(getActivity(), new String[] { file.toString() }, null,
+                                new MediaScannerConnection.OnScanCompletedListener() {
+                                    public void onScanCompleted(String path, Uri uri) {
+                                        mediaStoreUri = uri;
+                                        Log.v(TAG, "MediaStore Uri: "+uri);
+                                    }
+                                });
+                        takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, pictureFileUri);
+                        startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+                    }
+                }
+
             }
         });
 
         // Share picture
-        shareButton = (Button) rootview.findViewById(R.id.sharePicture);
+        shareButton = (ImageButton) rootview.findViewById(R.id.sharePicture);
         shareButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -72,34 +115,48 @@ public class FragmentCammar extends Fragment {
         });
         return rootview;
     }
-    private File getFile() {
-        File folder = new File("sdcard/camera_app");
-        if (!folder.exists()) {
-            folder.mkdir();
-
-        }
-        // Make a timestamp for image
-        String timestamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
-        File image_file = new File(folder, "WARUC_" + timestamp + ".jpg");
-
-        // For sharing the media (produces a Uri the Messenger has permissions for)
-        MediaScannerConnection.scanFile(getActivity(), new String[] { image_file.toString() }, null,
-                new MediaScannerConnection.OnScanCompletedListener() {
-                    public void onScanCompleted(String path, Uri uri) {
-                        mediaStoreUri = uri;
-                        Log.v(TAG, "MediaStore Uri: "+uri);
-                    }
-                });
-
-        return image_file;
-    }
 
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        String path = "sdcard/camera_app/cam_image.jpg";
-        imageView.setImageDrawable(Drawable.createFromPath(path));
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+//            String path = "sdcard/camera_app/" + fileName;
+            // TODO: 5/12/17
+            // Can show preview in view but it is sideways... NEED TO FIX!
+            ImageView imageView = (ImageView) getActivity().findViewById(R.id.CammarView);
+//            try {
+//                Log.e(TAG, "IMAGE URI: " + pictureFileUri);
+//                InputStream imageStream = getActivity().getContentResolver().openInputStream(pictureFileUri);
+//                Bitmap bitmap = createScaledBitmap(pictureFileUri.getPath(), imageView.getMaxWidth(), imageView.getHeight());
+//                imageView.setImageBitmap(bitmap);
+//            } catch (IOException ioe) {
+//                Log.e(TAG, Log.getStackTraceString(ioe));
+//            }
+            imageView.setImageURI(pictureFileUri);
+//            imageView.setImageResource(android.R.drawable.alert_dark_frame);
+            Log.e(TAG, "IMAGE URI: " + pictureFileUri);
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
 
+    public Bitmap createScaledBitmap(String pathName, int width, int height) {
+        final BitmapFactory.Options opt = new BitmapFactory.Options();
+        opt.inJustDecodeBounds = true;
+        BitmapFactory.decodeFile(pathName, opt);
+        opt.inSampleSize = calculateBmpSampleSize(opt, width, height);
+        opt.inJustDecodeBounds = false;
+        return BitmapFactory.decodeFile(pathName, opt);
+    }
 
+    public int calculateBmpSampleSize(BitmapFactory.Options opt, int width, int height) {
+        final int outHeight = opt.outHeight;
+        final int outWidth = opt.outWidth;
+        int sampleSize = 1;
+        if (outHeight > height || outWidth > width) {
+            final int heightRatio = Math.round((float) outHeight / (float) height);
+            final int widthRatio = Math.round((float) outWidth / (float) width);
+            sampleSize = heightRatio < widthRatio ? heightRatio : widthRatio;
+        }
+        return sampleSize;
     }
 
     // Share the picture
