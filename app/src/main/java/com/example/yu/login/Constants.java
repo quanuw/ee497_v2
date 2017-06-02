b22f9494fb8589f463aa694d1c0f84f07e5a1794
Quan Nguyen <quanuw@gmail.com>
2017-06-02 15:53:23 -0700

    Merge with master.

--- app/src/main/java/com/example/yu/login/Constants.java
+++ app/src/main/java/com/example/yu/login/Constants.java
@@ -12,7 +12,7 @@ import java.util.List;
 
 public class Constants {
     public static final long GEOFENCE_EXPIRATION_IN_MILLISECONDS = 12 * 60 * 60 * 1000;
-    public static final float GEOFENCE_RADIUS_IN_METERS = 100;
+    public static final float GEOFENCE_RADIUS_IN_METERS = 30;
 
     public static final HashMap<String, LatLng> LANDMARKS = new HashMap<String, LatLng>();
     static {
